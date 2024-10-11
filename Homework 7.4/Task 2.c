c96fb26e5f69683fa507e8053832b2b1f42f5739
Dim90Vas <48448669+Dim90Vas@users.noreply.github.com>
2024-10-11 17:30:40 +0700

    Create Task 2.c

---
+++ Homework 7.4/Task 2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Дан массив A[10]. Необходимо заполнить массив и вывести его максимальное значение, а также номер элемента максимального значения.*/
+int main() {
+    int A[10];
+    int max_ch=0, index;
+    printf("Fill the array: \n");
+    for(int i=0; i<10; ++i) {
+        scanf("%d",&A[i]);
+    }
+    for(int i=0; i<10; ++i) {
+        if(A[i]>max_ch) {
+            max_ch=A[i];
+            index=i;
+        }
+    }
+    printf("%d \t %d", max_ch, index);
+    return 0;
+}
