d42a22c72eb1853ffe4b5b6dd4546dd165eb49d0
Dim90Vas <48448669+Dim90Vas@users.noreply.github.com>
2025-05-02 22:19:27 +0700

    Update main.c

--- Lesson CubeMX/Lesson 6.5/1/main.c
+++ Lesson CubeMX/Lesson 6.5/1/main.c
@@ -1,33 +1,27 @@
 #include "main.h"
-
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-
 int main(void) {
   HAL_Init();
-
   SystemClock_Config();
-
   MX_GPIO_Init();
-	//настройка преоритета
+  //настройка преоритета
   SystemCoreClock = 8000000;
   HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
   HAL_InitTick(15);
-	//счет таймера
-	SysTick_Config(800000);
-	
-  while (1)
-  {
+  //счет таймера
+  SysTick_Config(SystemCoreClock/8);
+  while (1) {	
   }
 }
+
 //обработка прерывания
 void SysTick_Handler(void) {
-  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_13);
+  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_12);
   HAL_IncTick();
 }
 
-void SystemClock_Config(void)
-{
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
@@ -47,40 +41,31 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
     Error_Handler();
   }
 }
 
-static void MX_GPIO_Init(void)
-{
+static void MX_GPIO_Init(void) {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-
   __HAL_RCC_GPIOB_CLK_ENABLE();
-
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_RESET);
-
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
   /*Configure GPIO pin : PB13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Pin = GPIO_PIN_12;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 }
 
-void Error_Handler(void)
-{
+void Error_Handler(void) {
   __disable_irq();
-  while (1)
-  {
+  while (1)   {
   }
 }
 
 #ifdef  USE_FULL_ASSERT
-
-void assert_failed(uint8_t *file, uint32_t line)
-{
+void assert_failed(uint8_t *file, uint32_t line) {
 }
 #endif /* USE_FULL_ASSERT */
