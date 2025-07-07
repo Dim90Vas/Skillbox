9c26349bef61226a31f1148e71e21d3ecc56ef67
Dim90Vas <48448669+Dim90Vas@users.noreply.github.com>
2025-07-07 20:39:48 +0700

    Update main.c

--- Lesson CubeMX/Lesson 6.5/2/main.c
+++ Lesson CubeMX/Lesson 6.5/2/main.c
@@ -1,84 +1,118 @@
-#include "main.h"
+#include "stm32f1xx_hal.h"
+
+#define LED_PIN     GPIO_PIN_12
+#define LED_PORT    GPIOB
+#define TIM_HANDLE  htim1
+
 TIM_HandleTypeDef htim1;
+
+void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM1_Init(void);
+void Error_Handler(void);
 
 int main(void) {
-  HAL_Init();
-
-  MX_GPIO_Init();
-  MX_TIM1_Init();
- 
-	HAL_TIM_Base_Start_IT(&htim1);
-
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_SET);
-
-  while (1)
-  {
-  }
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
+    MX_TIM1_Init();
+    
+    // Настройка прерывания таймера
+    HAL_NVIC_SetPriority(TIM1_UP_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(TIM1_UP_IRQn);
+    
+    // Запуск таймера
+    HAL_TIM_Base_Start_IT(&TIM_HANDLE);
+    
+    while (1) {
+        // Основной цикл
+    }
 }
-//Обработка прерывания от таймера 
+
+// Обработчик прерывания TIM1
 void TIM1_UP_IRQHandler(void) {
-  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_13);
-  HAL_TIM_IRQHandler(&htim1);
+    if (__HAL_TIM_GET_FLAG(&TIM_HANDLE, TIM_FLAG_UPDATE) != RESET) {
+        __HAL_TIM_CLEAR_FLAG(&TIM_HANDLE, TIM_FLAG_UPDATE);
+        HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+    }
 }
 
+// Инициализация таймера TIM1
 static void MX_TIM1_Init(void) {
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
+    TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+    TIM_MasterConfigTypeDef sMasterConfig = {0};
 
-  htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 1000;
-  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 5000;
-  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim1.Init.RepetitionCounter = 0;
-  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    TIM_HANDLE.Instance = TIM1;
+    TIM_HANDLE.Init.Prescaler = 8000 - 1;    // 8 MHz / 8000 = 1 kHz
+    TIM_HANDLE.Init.CounterMode = TIM_COUNTERMODE_UP;
+    TIM_HANDLE.Init.Period = 1000 - 1;       // 1 kHz / 1000 = 1 Hz
+    TIM_HANDLE.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    TIM_HANDLE.Init.RepetitionCounter = 0;
+    TIM_HANDLE.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+    
+    if (HAL_TIM_Base_Init(&TIM_HANDLE) != HAL_OK) {
+        Error_Handler();
+    }
+    
+    sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+    if (HAL_TIM_ConfigClockSource(&TIM_HANDLE, &sClockSourceConfig) != HAL_OK) {
+        Error_Handler();
+    }
+    
+    sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+    if (HAL_TIMEx_MasterConfigSynchronization(&TIM_HANDLE, &sMasterConfig) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : PB13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
+// Инициализация GPIO
+static void MX_GPIO_Init(void) {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_TIM1_CLK_ENABLE();
+    
+    GPIO_InitStruct.Pin = LED_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
+    
+    HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_RESET);
 }
 
-void Error_Handler(void)
-{
-  __disable_irq();
-  while (1)
-  {
-  }
+// Конфигурация тактовой частоты
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+    
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
-#ifdef  USE_FULL_ASSERT
-
-void assert_failed(uint8_t *file, uint32_t line)
-{
+void Error_Handler(void) {
+    while(1) {
+        HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+        HAL_Delay(100);
+    }
 }
-#endif /* USE_FULL_ASSERT */
+
+
+
